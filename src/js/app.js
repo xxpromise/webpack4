34346131cd40ca20d13dee258b892d713df26677
xiongjian <webjsforyou@163.com>
2019-03-15 21:15:46 +0800

    'webpack开发环境的配置，可以在内存中自动打包所有类型文件并有自动编译运行、热更新等功能'

---
+++ src/js/app.js
@@ -0,0 +1,15 @@
+import mul from './module1';
+import Person from './module2';
+import count from './module3';
+import data from '../json/data';
+
+import '../less/test1.less'
+import '../less/test2.less'
+
+console.log(count(6, 4));
+console.log(mul(2, 3));
+console.log(Person);
+console.log(data);
+
+
+
