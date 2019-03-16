8ccac9379547ea49353b77d47049c36afc17703a
xiongjian <webjsforyou@163.com>
2019-03-16 21:09:55 +0800

    '完成生产环境基本配置'

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,10 +1,13 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
   entry: ['./src/js/app.js', './src/index.html'],
   output: {
     filename: 'js/[name].[hash:8].js',   //添加了hash值, 实现静态资源的长期缓存
-    publicPath: '/'  //所有输出资源公共路径
+    publicPath: '/'  //所有输出资源的公共路径
   },
   module: {
     rules: [
@@ -27,13 +30,12 @@ module.exports = {
           },
           {
             test: /\.less$/,
-            use: [{
-              loader: "style-loader"
-            }, {
-              loader: "css-loader"
-            }, {
-              loader: "less-loader"
-            }]
+            use: [
+              MiniCssExtractPlugin.loader,
+              'css-loader',
+              'postcss-loader',
+              'less-loader',
+            ]
           },
           {
             test: /\.(png|jpg|gif|svg)$/,
@@ -43,6 +45,30 @@ module.exports = {
                 options: {
                   limit: 8 * 1024,  // 8kb大小以下的图片文件都用base64处理
                   name: 'images/[name].[hash:8].[ext]'
+                }
+              },
+              {
+                loader: 'img-loader',
+                options: {
+                  plugins: [
+                    require('imagemin-gifsicle')({
+                      interlaced: false
+                    }),
+                    require('imagemin-mozjpeg')({
+                      progressive: true,
+                      arithmetic: false
+                    }),
+                    require('imagemin-pngquant')({
+                      floyd: 0.5,
+                      speed: 2
+                    }),
+                    require('imagemin-svgo')({
+                      plugins: [
+                        { removeTitle: true },
+                        { convertPathData: false }
+                      ]
+                    })
+                  ]
                 }
               }
             ]
@@ -66,7 +92,29 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: './src/index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      }
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].[hash:8].css",
+      chunkFilename: "css/[id].[hash:8].css"
+    }),
+    new CleanWebpackPlugin(),
+    new OptimizeCssAssetsPlugin({
+      cssProcessorPluginOptions: {
+        preset: ['default', { discardComments: { removeAll: true } }],
+      },
     })
   ],
   mode: 'production',  //修改为生产环境
